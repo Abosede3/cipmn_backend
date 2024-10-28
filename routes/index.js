29c2c90fe0bcc418723c587811b8a8ce64cb6ca7
Adedoyin Seyi Emmanuel <53402704+Nuelsville@users.noreply.github.com>
2024-10-28 11:50:32 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,19 @@ const router = express.Router();
 
 const authRoutes = require('./auth');
 // const userRoutes = require('./users');
-// const votingYearRoutes = require('./votingYears');
-// const positionRoutes = require('./positions');
-// const candidateRoutes = require('./candidates');
-// const voteRoutes = require('./votes');
+const votingYearRoutes = require('./votingYears');
+const positionRoutes = require('./positions');
+const candidateRoutes = require('./candidates');
+const voteRoutes = require('./votes');
 
 
 
 router.use('/api/auth', authRoutes);
 // router.use('/api/users', userRoutes);
-// router.use('/api/voting-years', votingYearRoutes);
-// router.use('/api/positions', positionRoutes);
-// router.use('/api/candidates', candidateRoutes);
-// router.use('/api/votes', voteRoutes);
+router.use('/api/voting-years', votingYearRoutes);
+router.use('/api/positions', positionRoutes);
+router.use('/api/candidates', candidateRoutes);
+router.use('/api/votes', voteRoutes);
 
 
 
